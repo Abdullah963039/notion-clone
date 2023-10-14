e832ffa08992c0e86d04fc20b7f2c8deec9f5e47
AbdullahAlWaraa <103293258+Abdullah963039@users.noreply.github.com>
2023-10-14 20:54:12 +0300

    add trash box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,9 +5,11 @@ import { toast } from "sonner";
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { useMediaQuery } from "usehooks-ts";
 import { usePathname } from "next/navigation";
@@ -15,10 +17,16 @@ import { useMutation } from "convex/react";
 
 import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
+import { TrashBox } from "./trash-box";
 
 export function Navigation() {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -151,6 +159,18 @@ export function Navigation() {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item label="Add a page" onClick={handleCreate} icon={Plus} />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              side={isMobile ? "bottom" : "right"}
+              className="p-0 w-72"
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 top-0 right-0"
